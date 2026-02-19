4ab301433d9c6c407537a0047bc735a97293e7fc
Kari Tsuki <peiblo_1472@live.com.mx>
2026-02-19 11:55:53 -0600

    feat: implementación inicial de la clase Hotel y persistencia JOSN

---
+++ source/hotel.py
@@ -0,0 +1,12 @@
+"""
+hotel.py
+
+Este programa gestiona la clase Hotel. Incluye funciones de
+creación, eliminación, muestreo, modificacion, reserva y
+cancelación de reserva.
+"""
+
+import json
+import os
+
+
