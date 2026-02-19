6cf593cf7cb5a177ecd646583e8d06f4271090f9
Kari Tsuki <peiblo_1472@live.com.mx>
2026-02-19 12:02:09 -0600

    feat: Incorporacion de la funcion de inicialización de la clase Hotel

--- source/hotel.py
+++ source/hotel.py
@@ -10,3 +10,15 @@ import json
 import os
 
 
+class Hotel:
+    """Clase para la administración de los hoteles"""
+
+    def __init__(self, hotel_id, name, location, rooms):
+        return{
+            "hotel_id": self.hotel_id,
+            "name": self.name,
+            "location": self.location,
+            "rooms": self.rooms
+        }
+    
+    
