c3fe2b8dceb199fee105194f1a9b4342b9562675
Kari Tsuki <peiblo_1472@live.com.mx>
2026-02-19 12:06:47 -0600

    feat: Incorporacion de la funcion 'to_dict' de la clase Hotel

--- source/hotel.py
+++ source/hotel.py
@@ -14,6 +14,14 @@ class Hotel:
     """Clase para la administración de los hoteles"""
 
     def __init__(self, hotel_id, name, location, rooms):
+        "Se crea un nuevo objeto 'Hotel'"
+        self.hotel_id = hotel_id
+        self.name = name
+        self.location = location
+        self.rooms = rooms
+    
+    def to_dict(self):
+        """Convierte el objeto 'Hotel' en un diccionario"""
         return{
             "hotel_id": self.hotel_id,
             "name": self.name,
